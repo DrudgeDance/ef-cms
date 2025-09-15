43b5b89904964c2b8905f83b8b4cd565c64115db
Cody Seibert <cseibert113@gmail.com>
2025-09-15 11:12:43 -0400

    9036: improve the maintence mode logic to catch uncaught exceptions to return proper maintence mode value to response

--- web-api/src/persistence/postgres/featureFlag/getMaintenanceMode.ts
+++ web-api/src/persistence/postgres/featureFlag/getMaintenanceMode.ts
@@ -3,15 +3,29 @@ import { getFeatureFlagValues } from '@web-api/persistence/postgres/featureFlag/
 export function getMaintenanceMode(): Promise<
   { current: boolean } | undefined
 > {
-  return getFeatureFlagValues(['maintenance-mode'])
-    .then(POSTGRES_RECORDS => {
-      if (!POSTGRES_RECORDS) return { current: false };
-      if (!POSTGRES_RECORDS.length) return { current: false };
-      const MAINTENANCE_RECORD = POSTGRES_RECORDS[0];
-      return MAINTENANCE_RECORD.value;
-    })
-    .catch(() => {
-      // if we can't connect to postgres, we assume maintence mode on due to critical issues
-      return { current: true };
-    });
+  return new Promise((resolve) => {
+    const onPgConnectionError = () => {
+      // the database threw some type of connection error, assume maintence mode on due to critical issues
+      resolve({ current: true });
+    };
+
+    // this is only possible in a serverless environment of one request per lambda,
+    // on a hosted node stateful service, this might cause side effects.
+    process.on('uncaughtException', onPgConnectionError);
+
+    getFeatureFlagValues(['maintenance-mode'])
+      .then(POSTGRES_RECORDS => {
+        if (!POSTGRES_RECORDS) resolve({ current: false });
+        if (!POSTGRES_RECORDS.length) resolve({ current: false });
+        const MAINTENANCE_RECORD = POSTGRES_RECORDS[0];
+        resolve(MAINTENANCE_RECORD.value);
+      })
+      .catch(() => {
+        // if we can't connect to postgres, we assume maintence mode on due to critical issues
+        resolve({ current: true });
+      }).finally(() => {
+        process.off('uncaughtException', onPgConnectionError);
+      });
+
+  })
 }
